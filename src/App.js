2a0c2da64fcde84b8f1cf0a56262edf5f532f8fb
anhengchangyua <188851312@qq.com>
2019-02-19 01:10:52 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import app from './app.png'
 
 const App = () => {
   return (
     <div>
-      <p className="dd">React here!1111</p>
+      <p className="dd">React here!</p>
+      <img src={app} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
+const dev = require('./webpack/webpack.dev.js')
+const prod = require('./webpack/webpack.prod.js')
 
-module.exports = {
-  mode: 'development',
-  // entry: './src/index.js',
-  // entry: { app: ['@babel/polyfill', './src/index.js'], hello: './src/hello.js' },
-  entry: { app: './src/index.js', hello: './src/hello.js' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    // filename: '[name].bundle.js',
-    filename: '[name]-[hash].bundle.js',
-  },
-  devtool: false, //开发者模式
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          // options: {
-          // presets: [
-          //   [
-          //     '@babel/preset-env',
-          //     {
-          //       debug: true, //查看安装的插件
-          //     },
-          //   ],
-          // ],
-          // plugins: [['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }]],
-          // },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(scss|sass)$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader, // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader, // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'less-loader', // compiles Less to CSS
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Admin',
-      filename: 'admin.html',
-      template: 'public/index.html',
-      chunks: ['hello'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Index',
-      filename: 'index.html',
-      template: 'public/index.html',
-      chunks: ['app'],
-    }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    // host: '0.0.0.0',
-  },
-};
+const TARGET = process.env.npm_lifecycle_event
+
+if (TARGET === 'dev') {
+  module.exports = dev
+}
+
+if (TARGET === 'prod') {
+  module.exports = prod
+}
